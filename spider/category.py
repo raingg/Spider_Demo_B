57fd3d3151a0a1f76a01541e33b5a7236c5040c3
thu <mingfei.net@gmail.com>
2018-12-25 17:20:21 +0800

    Update

--- spider/category.py
+++ spider/category.py
@@ -12,6 +12,7 @@
 import requests as req
 import pandas as pd
 from pathlib import Path
+import mysql.connector
 
 link = 'http://you.163.com/xhr/globalinfo//queryTop.json'
 
@@ -38,10 +39,37 @@ columns = ['id', 'title', 'group', 'desc', 'icon', 'categoryId']
 cate_pd = pd.DataFrame(categories, columns=columns)
 cate_pd.to_csv(Path(__file__).parents[1].joinpath('data', 'csv', 'category.csv'), encoding='UTF-8', index=False)
 
-
 '''
 # csv:
 id, title, group, desc, icon, categoryId
 1005000,居家,,,,
 1008009,床品件套,床品,MUJI等品牌制造商出品,785a1507ce654746875063805c6c4235.png,1005000
 '''
+
+connection = mysql.connector.connect(
+    user='root',
+    password='system'
+)
+
+cursor = connection.cursor()
+
+cursor.execute('set foreign_key_checks = 0')  # 临时解除外键约束
+cursor.execute('truncate table db_b.category')
+cursor.execute('set foreign_key_checks = 1')  # 再次启用外键约束
+
+sql = """
+load data local infile 'D:/PycharmProjects/Spider_Demo_B/data/csv/category.csv'
+into table db_b.category
+fields terminated by ','
+ignore 1 lines
+(id, title, @v_group, @v_desc, @v_icon, @v_categoryId)
+set
+`group` = nullif(@v_group, ''),
+`desc` = nullif(@v_desc, ''),
+icon = nullif(@v_icon, ''),
+categoryId = nullif(@v_categoryId, '')
+"""
+
+cursor.execute(sql)
+
+connection.commit()
---
+++ spider/product.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# @Time : 12/25/2018 17:18
+# @Author : mingfei.net@gmail.com
+# @FileName : product.py
+# @GitHub : https://github.com/thu/Spider_Demo_B
+
+
+
+
+'''
+csv
+
+sup_id, sub_id, product_id
+1005000,1008009,3413004
+...
+5k
+'''
