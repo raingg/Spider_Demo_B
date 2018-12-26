926f4053083d365dabc7aa4dfbe0b8f9072d7ceb
thu <mingfei.net@gmail.com>
2018-12-26 11:41:56 +0800

    Update

--- spider/product.py
+++ spider/product.py
@@ -5,6 +5,33 @@
 # @FileName : product.py
 # @GitHub : https://github.com/thu/Spider_Demo_B
 
+import requests as req
+import json
+
+
+# todo 获取所有一级类目 id
+
+sup_ids = []
+
+
+def get_csv(sup_id):
+    url = 'http://you.163.com/item/list?categoryId=' + str(sup_id)
+    for line in req.get(url).iter_lines():
+        line = line.decode('utf-8')
+        if line.startswith('var json_Data='):
+            data = json.loads(line[len('var json_Data='):-1])
+            print(type(data))
+            categoryItemList = data['categoryItemList']
+            print(categoryItemList)
+
+            # sub_id
+            # product_id
+
+
+get_csv(1005000)
+# for sup_id in sup_ids:
+#     get_csv(sup_id)
+
 
 
 
